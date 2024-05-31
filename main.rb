7f692a9b39d4cd89c285370a32332c05b1337f1b
patovera <verapatricio9@gmail.com>
2024-05-31 15:40:51 -0300

    feat: implemented functionality to check right places and digits in a guess

--- main.rb
+++ main.rb
@@ -2,5 +2,11 @@
 
 require './mastermind'
 
-puts "The CodeMaker has chosen a 4 digit code for you to break \nYou have 15 turns to break it"
-
+puts "The CodeMaker has chosen a 4 digit code for you to break \nYou have 15 attemps to break it"
+game =  MasterMindGame.new(rand(1000 .. 9999))
+game.play
+guess = game.get_guess
+print game.check_right_places(guess)
+puts
+print game.check_right_digits(guess)
+puts
--- mastermind.rb
+++ mastermind.rb
@@ -1,4 +1,55 @@
 
+class MasterMindGame
+
+  attr_accessor :attemps, :rigth_places, :right_digits
+
+  def initialize(code)
+    @code = code.to_s.split("")
+    @attemps = 1
+    @rigth_places = 0
+    @right_digits = 0
+  end
+
+  def get_guess()
+    puts 'Select your guess (4 digit long): '
+    guess = gets.to_s.chomp.split("")
+    while guess.length != 4
+      puts 'Invalid guess (Must be a 4 digit number)'
+      guess = gets.to_s.chomp.split("")
+    end
+    return guess
+  end
+
+  def check_guess(guess)
+    @code == guess
+  end
+
+  def check_right_places(guess)
+    guess.length.times do |i|
+      if (guess[i] == @code[i])
+        @rigth_places += 1
+      end
+    end
+    return @rigth_places
+  end
+
+
+  ## ARREGLAR ESTO
+  def check_right_digits(guess)
+    guess.length.times do |i|
+      if (@code.include?(guess[i]) && @code[i] != guess[i])
+        @right_digits += 1
+      end
+    end
+    return @right_digits
+  end
+
+  def play
+    print @code
+    puts 
+  end
+
+end
 
 
 
